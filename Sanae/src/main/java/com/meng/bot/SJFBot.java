28def51c07e9ee3228818ec80bf5c36f1a35a9ab
swordarrow2 <2856986197@qq.com>
2020-09-13 18:31:37 +0800

    z

---
+++ Sanae/src/main/java/com/meng/bot/SJFBot.java
@@ -0,0 +1,69 @@
+package com.meng.bot;
+
+import java.io.File;
+import java.util.List;
+import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.BotFactoryJvm;
+import net.mamoe.mirai.event.Events;
+import net.mamoe.mirai.event.ListenerHost;
+import net.mamoe.mirai.utils.BotConfiguration;
+
+/**
+ * @Description: 创建bot
+ * @author: magic chen
+ * @date: 2020/8/20 15:46
+ **/
+public class SJFBot {
+
+    private Bot bot;
+
+    public SJFBot(Long account, String pwd, String deviceInfo, List<ListenerHost> events, String netlog) {
+        bot = startBot(account, pwd, deviceInfo, events, netlog);
+    }
+
+    public Bot getBot() {
+        return bot;
+    }
+
+    public void setBot(Bot bot) {
+        this.bot = bot;
+    }
+
+    /**
+     * @Name: startBot
+     * @Description: 创建bot
+     * @Param: account bot账号
+     * @Param: pwd  bot密码
+     * @Param: deviceInfo 存储设备信息文件
+     * @Param: events 注册监听事件
+     * @Param: netlog net日志重定向到文件夹路径
+     * @Return: net.mamoe.mirai.Bot
+     * @Author: magic chen
+     * @Date: 2020/8/20 15:54
+     **/
+    private Bot startBot(Long account, String pwd, String deviceInfo, List<ListenerHost> events, String netlog) {
+        BotConfiguration config = new BotConfiguration();
+        config.fileBasedDeviceInfo(deviceInfo);
+        // 使用自定义的log
+       // config.setBotLoggerSupplier(bot -> new MagicLogger());
+        // 将net层输出写入文件
+        config.redirectNetworkLogToDirectory(new File(netlog));
+        final Bot bot = BotFactoryJvm.newBot(account, pwd, config);
+        bot.login();
+        // 注册事件
+        for (ListenerHost event : events) {
+            Events.registerEvents(bot, event);
+        }
+        // 这个和picbotx 还是不太一样 那个不会占用主线程
+        // 这里必须要启新线程去跑bot 不然会占用主线程
+        new Thread(new Runnable(){
+
+                @Override
+                public void run() {
+                    bot.join(); 
+                }
+            }).start();
+        return bot;
+    }
+
+}
